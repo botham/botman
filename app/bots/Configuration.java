d5e162fdbbb98f5a6c255c92d558b41495487b04
ulasakdeniz <akdenizulas@gmail.com>
2016-11-01 19:55:01 +0200

    text extraction is fixed and configuration is formatted

--- app/bots/Configuration.java
+++ app/bots/Configuration.java
@@ -3,14 +3,12 @@ package bots;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-import java.util.Map;
-
 public class Configuration {
 
     private static final Config config = ConfigFactory.load();
-    static final String telegramBotToken = System.getenv("TELEGRAM-BOT-TOKEN");
-    static final String facebookBotToken = System.getenv("FACEBOOK-BOT-TOKEN");
-    public static final String facebookBotVerification = System.getenv("FACEBOOK-BOT-VERIFICATION");
+    static final String telegramBotToken = System.getenv("TELEGRAM_BOT_TOKEN");
+    static final String facebookBotToken = System.getenv("FACEBOOK_BOT_TOKEN");
+    public static final String facebookBotVerification = System.getenv("FACEBOOK_BOT_VERIFICATION");
     static final String telegramBotName = config.getString("telegram.bot-name");
 
 }
--- app/models/Message.java
+++ app/models/Message.java
@@ -4,37 +4,34 @@ import java.util.Set;
 
 public class Message {
 
-    private String sender, text, senderClient, recipientClient;
-    private Set<String> recipients;
+  private String sender, text, senderClient;
+  private Set<String> recipients;
 
-    public Message(String sender, String senderClient, Set<String> recipients, String text) {
-        this.sender = sender;
-        this.senderClient = senderClient;
-        this.recipients = recipients;
-        this.text = text;
-    }
+  public Message(String sender, String senderClient, Set<String> recipients, String text) {
+    this.sender = sender;
+    this.senderClient = senderClient;
+    this.recipients = recipients;
+    this.text = text;
+  }
 
-    //empty constructor for Jackson
-    public Message() {}
+  //empty constructor for Jackson
+  public Message() {
+  }
 
-    public String getSender() {
-        return sender;
-    }
+  public String getSender() {
+    return sender;
+  }
 
-    public Set<String> getRecipients() {
-        return recipients;
-    }
+  public Set<String> getRecipients() {
+    return recipients;
+  }
 
-    public String getText() {
-        return text;
-    }
+  public String getText() {
+    return text;
+  }
 
-    public String getSenderClient() {
-        return senderClient;
-    }
-
-    public String getRecipientClient() {
-        return recipientClient;
-    }
+  public String getSenderClient() {
+    return senderClient;
+  }
 
 }
